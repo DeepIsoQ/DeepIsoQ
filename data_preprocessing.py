61072f0e0147dc09e1a7461c89f87a42a3b7b2c4
jorgeesant <s243310@dtu.dk>
2025-11-10 14:25:53 +0100

    functional script!!

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,23 +6,18 @@ from scipy import sparse
 
 GENE_H5AD = "/work3/s193518/scIsoPred/data/bulk_processed_genes.h5ad"
 TX_H5AD   = "/work3/s193518/scIsoPred/data/bulk_processed_transcripts.h5ad"
-OUT_PT    = "processed_data_nofilter.pt"
+
 
 def to_dense_f32(x):
+    if hasattr(x, "to_memory"):     
+        x = x.to_memory()        
     if sparse.issparse(x):
-        return x.tocsr().astype(np.float32).toarray()
+        return x.astype(np.float32).toarray()
     return np.asarray(x, dtype=np.float32)
 
 # Load and read data (takes around 8 min)
-gene_ad = ad.read_h5ad(GENE_H5AD, backed = "r")
-tx_ad   = ad.read_h5ad(TX_H5AD, backed = "r")
-
-# Make sure that the samples in both datasets match (otherwise not comparable)
-common = gene_ad.obs_names.intersection(tx_ad.obs_names)
-if len(common) == 0:
-    raise ValueError("No overlapping samples between gene and transcript AnnData.")
-gene_ad = gene_ad[common].copy()
-tx_ad   = tx_ad[common].copy()
+gene_ad = ad.read_h5ad(GENE_H5AD, backed = 'r')
+tx_ad   = ad.read_h5ad(TX_H5AD, backed ='r')
 
 # Transform data to tensors
 X_gene = torch.tensor(to_dense_f32(gene_ad.X), dtype=torch.float32)  # (N, G)
@@ -33,7 +28,7 @@ gene_ids = (gene_ad.var["gene_id"].astype(str).tolist()
             if "gene_id" in gene_ad.var.columns else gene_ad.var_names.astype(str).tolist())
 tx_ids   = tx_ad.var_names.astype(str).tolist()
 
-# 5) inputs for model = log1p genes (you’ll standardize on the train split later)
+# log1p genes
 Xg_log1p = torch.log1p(X_gene)
 Y_tx     = X_tx  # targets = absolute isoform expression, all transcripts kept
 
@@ -49,4 +44,4 @@ print(f"Isoforms:{Y_tx.shape[1]}")
 #    "tx_ids": tx_ids,       # list of I
 #}, OUT_PT)
 
-print("Processed (no-filter) data saved to:", OUT_PT)
+
