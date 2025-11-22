559e2a3124a6ac7e81e48322052f65d954b65066
er-biotecazu <torresgarciaeric@gmail.com>
2025-11-22 16:35:22 +0100

    changed name

---
+++ VAE/variational_autoencoder.py
@@ -0,0 +1,385 @@
+#!/usr/bin/env python3
+"""
+Variational Autoencoder for Isoform Expression Prediction
+"""
+# Commented out IPython magic to ensure Python compatibility.
+from typing import *
+from plotting import make_vae_plots
+import matplotlib
+import matplotlib.pyplot as plt
+from IPython.display import Image, display, clear_output
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import math
+import torch
+import os 
+from torch import nn, Tensor
+from torch.nn.functional import softplus
+from torch.distributions import Distribution
+#from torchvision.transforms import ToTensor
+from functools import reduce
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Subset, DataLoader, TensorDataset
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+sns.set_style("whitegrid")
+
+# ------------------------------
+# Config
+# ------------------------------
+SEED            = 42
+TEST_FRAC       = 0.15
+VAL_FRAC        = 0.15
+DEVICE          = "cuda" if torch.cuda.is_available() else "cpu"
+AMP             = (DEVICE == "cuda")
+
+
+class ReparameterizedDiagonalGaussian(Distribution):
+    """
+    A distribution `N(y | mu, sigma I)` compatible with the reparameterization trick given `epsilon ~ N(0, 1)`.
+    """
+    def __init__(self, mu: Tensor, log_sigma:Tensor):
+        assert mu.shape == log_sigma.shape, f"Tensors `mu` : {mu.shape} and ` log_sigma` : {log_sigma.shape} must be of the same shape"
+        self.mu = mu
+        self.sigma = log_sigma.exp()
+
+    def sample_epsilon(self) -> Tensor:
+        """`eps ~ N(0, I)`"""
+        # creates an empty tensor with the same shape as mu and fills it with values from a N(0, 1)
+        return torch.empty_like(self.mu).normal_()
+
+    def sample(self) -> Tensor:
+        """sample `z ~ N(z | mu, sigma)` (without gradients)"""
+        with torch.no_grad():
+            return self.rsample()
+
+    def rsample(self) -> Tensor:
+        """sample `z ~ N(z | mu, sigma)` (with the reparameterization trick) """
+        return self.mu + self.sigma * self.sample_epsilon()
+
+    def log_prob(self, z:Tensor) -> Tensor:
+        """return the log probability: log `p(z)`"""
+        return -0.5 * (
+            ((z - self.mu) ** 2) / (self.sigma ** 2)
+            + 2 * self.sigma.log()
+            + math.log(2 * math.pi)
+        )
+
+
+
+
+# ------------------------------
+# Data path (portable)
+# ------------------------------
+DATA_PT = os.environ.get("DATA_PT")
+
+if DATA_PT is None:
+    # Fall back to $BLACKHOLE/$USER/data.pt
+    bh = os.environ.get("BLACKHOLE")
+    user = os.environ.get("USER")
+    if bh is None or user is None:
+        raise RuntimeError(
+            "DATA_PT not set and BLACKHOLE/USER env vars missing. "
+            "Either export DATA_PT or run on DTU HPC where BLACKHOLE & USER are defined."
+        )
+    DATA_PT = os.path.join(bh, user, "data.pt")
+
+print(f"[INFO] Loading tensors from: {DATA_PT}")
+data = torch.load(DATA_PT, map_location="cpu", weights_only=False)
+
+
+X = data["Xg_log1p"].float().cpu()
+#Y = torch.log1p(data["Y_tx"].float()).cpu()
+
+
+N, G = X.shape
+#_, I = Y.shape
+print(f"[INFO] Shapes: X={X.shape}")
+
+
+# Define the train, test and validation sets
+all_idx = np.arange(N)
+trval_idx, te_idx = train_test_split(all_idx, test_size=TEST_FRAC, random_state=SEED, shuffle=True)
+val_rel = VAL_FRAC / (1.0 - TEST_FRAC)
+tr_idx, va_idx = train_test_split(trval_idx, test_size=val_rel, random_state=SEED, shuffle=True)
+
+
+# ------------------------------
+# Create datasets
+# ------------------------------
+full_dataset = TensorDataset(X)
+
+dset_train = Subset(full_dataset, trval_idx)
+dset_test  = Subset(full_dataset, te_idx)
+#dset_val   = Subset(full_dataset, va_idx)
+
+# ------------------------------
+# DataLoaders
+# ------------------------------
+
+batch_size = 64
+eval_batch_size = 128
+
+# A stratified sampler is a sampling method that builds batches 
+# while preserving the class proportions of the original dataset.
+# Unsupervised learning: no class labels.
+
+train_loader = DataLoader(dset_train, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dset_test, batch_size=eval_batch_size, shuffle=False)
+#val_loader = DataLoader(dset_val, batch_size=eval_batch_size, shuffle=False)
+
+print(f"[INFO] Loader sizes: train={len(train_loader)}  val={len(test_loader)}")
+
+"""
+Building the model
+When defining the model the latent layer must act as a bottleneck of information, so that we ensure that we find a strong internal representation. We initialize the VAE with 1 hidden layer in the encoder and decoder using relu units as non-linearity.
+"""
+
+class VariationalAutoencoder(nn.Module):
+    """A Variational Autoencoder with
+    * a Gaussian likelihood observation model `p_\theta(x | z) = N(x | \mu_\theta(z), \sigma^2_\theta(z) I)`
+    * a Gaussian prior `p(z) = N(z | 0, I)`
+    * a Gaussian posterior `q_\phi(z|x) = N(z | \mu(x), \sigma(x))`
+    """
+
+    def __init__(self, input_shape:torch.Size, latent_features:int) -> None:
+        super(VariationalAutoencoder, self).__init__()
+
+        self.input_shape = input_shape
+        self.latent_features = latent_features
+        self.observation_features = np.prod(input_shape)
+
+
+        # Inference Network
+        # Encode the observation `x` into the parameters of the posterior distribution
+        # `q_\phi(z|x) = N(z | \mu(x), \sigma(x)), \mu(x),\log\sigma(x) = h_\phi(x)`
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features=self.observation_features, out_features=256),
+            nn.ReLU(),
+            nn.Linear(in_features=256, out_features=128),
+            nn.ReLU(),
+            # A Gaussian is fully characterised by its mean \mu and variance \sigma**2
+            nn.Linear(in_features=128, out_features=2*latent_features) # <- note the 2*latent_features
+        )
+
+        # Generative Model
+        # Decode the latent sample `z` into the parameters of the observation model
+        # `p_\theta(x | z) = \prod_i B(x_i | g_\theta(x))`
+        self.decoder = nn.Sequential(
+            nn.Linear(in_features=latent_features, out_features=128),
+            nn.ReLU(),
+            nn.Linear(in_features=128, out_features=256),
+            nn.ReLU(),
+            nn.Linear(in_features=256, out_features=2*self.observation_features)
+        )
+
+        # define the parameters of the prior, chosen as p(z) = N(0, I)
+        self.register_buffer('prior_params', torch.zeros(torch.Size([1, 2*latent_features])))
+
+    def posterior(self, x:Tensor) -> Distribution:
+        """return the distribution `q(z|x) = N(z | \mu(x), \sigma(x))`"""
+
+        # compute the parameters of the posterior
+        h_x = self.encoder(x)
+        mu, log_sigma =  h_x.chunk(2, dim=-1)
+
+        # return a distribution `q(z|x) = N(z | \mu(x), \sigma(x))`
+        return ReparameterizedDiagonalGaussian(mu, log_sigma)
+
+    def prior(self, batch_size:int=1)-> Distribution:
+        """return the distribution `p(z)`"""
+        prior_params = self.prior_params.expand(batch_size, *self.prior_params.shape[-1:])
+        mu, log_sigma = prior_params.chunk(2, dim=-1)
+
+        # return the distribution `p(z)`
+        return ReparameterizedDiagonalGaussian(mu, log_sigma)
+
+    def observation_model(self, z:Tensor) -> Distribution:
+        """return the distribution `p(x|z)`"""
+        obs_params = self.decoder(z)
+        mu, log_sigma = obs_params.chunk(2, dim=-1)
+        # reshape the output to the input shape
+        mu = mu.view(-1, *self.input_shape) 
+        log_sigma = log_sigma.view(-1, *self.input_shape)
+        return ReparameterizedDiagonalGaussian(mu, log_sigma)
+
+    def forward(self, x) -> Dict[str, Any]:
+        """compute the posterior q(z|x) (encoder), sample z~q(z|x) and return the distribution p(x|z) (decoder)"""
+
+        # flatten the input
+        x = x.view(x.size(0), -1)
+
+        # define the posterior q(z|x) / encode x into q(z|x)
+        qz = self.posterior(x)
+
+        # define the prior p(z)
+        pz = self.prior(batch_size=x.size(0))
+
+        # sample the posterior using the reparameterization trick: z ~ q(z | x)
+        z = qz.rsample()
+
+        # define the observation model p(x|z) = N(x | g(z))
+        px = self.observation_model(z)
+
+        return {'px': px, 'pz': pz, 'qz': qz, 'z': z}
+
+
+    def sample_from_prior(self, batch_size:int=128):
+        """sample z~p(z) and return p(x|z)"""
+
+        # degine the prior p(z)
+        pz = self.prior(batch_size=batch_size)
+
+        # sample the prior
+        z = pz.rsample()
+
+        # define the observation model p(x|z) = N(x | g(z))
+        px = self.observation_model(z)
+
+        return {'px': px, 'pz': pz, 'z': z}
+
+# initialize the VAE
+latent_features = 32
+vae = VariationalAutoencoder(torch.Size([G]), latent_features)
+
+"""
+Implementation of the ELBO and beta ELBO
+"""
+
+def reduce(x:Tensor) -> Tensor:
+    """for each datapoint: sum over all dimensions"""
+    return x.view(x.size(0), -1).sum(dim=1)
+
+class VariationalInference(nn.Module):
+    def __init__(self, beta:float=1.):
+        super().__init__()
+        self.beta = beta
+
+    def forward(self, model:nn.Module, x:Tensor) -> Tuple[Tensor, Dict]:
+
+        # forward pass through the model
+        outputs = model(x)
+
+        # unpack outputs
+        px, pz, qz, z = [outputs[k] for k in ["px", "pz", "qz", "z"]]
+
+        # evaluate log probabilities
+        log_px = reduce(px.log_prob(x))
+        log_pz = reduce(pz.log_prob(z))
+        log_qz = reduce(qz.log_prob(z))
+
+        # compute the ELBO with and without the beta parameter:
+        # `L^\beta = E_q [ log p(x|z) ] - \beta * D_KL(q(z|x) | p(z))`
+        # where `D_KL(q(z|x) | p(z)) = log q(z|x) - log p(z)`
+        kl = log_qz - log_pz
+        elbo =  log_px - kl 
+        beta_elbo = log_px - self.beta * kl 
+
+        # loss
+        loss = -beta_elbo.mean()
+
+        # prepare the output
+        with torch.no_grad():
+            diagnostics = {'elbo': elbo, 'log_px':log_px, 'kl': kl}
+
+        return loss, diagnostics, outputs
+
+vi = VariationalInference(beta=1.0)
+loss, diagnostics, outputs = vi(vae, X)
+print(f"{'loss':6} | mean = {loss:10.3f}, shape: {list(loss.shape)}")
+for key, tensor in diagnostics.items():
+    print(f"{key:6} | mean = {tensor.mean():10.3f}, shape: {list(tensor.shape)}")
+
+"""## Training and Evaluation
+
+### Initialize the model, evaluator and optimizer
+"""
+
+from collections import defaultdict
+# define the models, evaluator and optimizer
+
+# Evaluator: Variational Inference
+beta = 1
+vi = VariationalInference(beta=beta)
+
+# The Adam optimizer works really well with VAEs.
+optimizer = torch.optim.Adam(vae.parameters(), lr=1e-3)
+
+# define dictionary to store the training curves
+training_data = defaultdict(list)
+validation_data = defaultdict(list)
+
+
+epoch = 0
+
+"""### Training Loop
+
+**plotting guide**:
+
+* 1st row: Reproducing the figure from the begining of the Notebook.
+    * (Left) Data.
+    * (Middle) Latent space: the large gray disk reprensents the prior (radius = $2\sigma$), each point represents a latent sample $\mathbf{z}$. The smaller ellipses represent the distributions $q_\phi(\mathbf{z} | \mathbf{x})$  (radius = $2\sigma$). When using $\geq 2$ latent features, dimensionality reduction is applied using t-SNE and only samples $\mathbf{z} \sim q_\phi(\mathbf{z} | \mathbf{x})$ are displayed.
+    * (Right) samples from $p_\theta(\mathbf{x} | \mathbf{z})$.
+
+* 2nd row: Training curves
+
+* 2rd row: Latent samples.
+    * (Left) Prior samples $\mathbf{x} \sim p_\theta(\mathbf{x} | \mathbf{z}), \mathbf{z} \sim p(\mathbf{z})$
+    * (Middle) Latent Interpolations. For each row: $\mathbf{x} \sim p_\theta(\mathbf{x} | t \cdot \mathbf{z}_1 + (1-t) \cdot \mathbf{z}_2), \mathbf{z}_1, \mathbf{z}_2 \sim p(\mathbf{z}), t=0 \dots 1$.
+    * (Right): Sampling $\mathbf{z}$ from a grid [-3:3, -3:3] $\mathbf{x} \sim p_\theta(\mathbf{x} | \mathbf{z}), \mathbf{z} \sim \operatorname{grid}(-3:3, -3:3)$ (only available for 2d latent space).
+
+**NOTE** this will take a while on CPU.
+"""
+
+num_epochs = 100
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f">> Using device: {device}")
+
+# move the model to the device
+vae = vae.to(device)
+
+# training..
+while epoch < num_epochs:
+    epoch+= 1
+    training_epoch_data = defaultdict(list)
+    vae.train()
+
+    # Go through each batch in the training dataset using the loader
+    # Note that y is not necessarily known as it is here
+    for x, y in train_loader:
+        x = x.to(device)
+
+        # perform a forward pass through the model and compute the ELBO
+        loss, diagnostics, outputs = vi(vae, x)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # gather data for the current bach
+        for k, v in diagnostics.items():
+            training_epoch_data[k] += [v.mean().item()]
+
+
+    # gather data for the full epoch
+    for k, v in training_epoch_data.items():
+        training_data[k] += [np.mean(training_epoch_data[k])]
+
+    # Evaluate on a single batch, do not propagate gradients
+    with torch.no_grad():
+        vae.eval()
+
+        # Just load a single batch from the test loader
+        x, y = next(iter(test_loader))
+        x = x.to(device)
+
+        # perform a forward pass through the model and compute the ELBO
+        loss, diagnostics, outputs = vi(vae, x)
+
+        # gather data for the validation step
+        for k, v in diagnostics.items():
+            validation_data[k] += [v.mean().item()]
+
+    # Reproduce the figure from the begining of the notebook, plot the training curves and show latent samples
+    make_vae_plots(vae, x, y, outputs, training_data, validation_data)
